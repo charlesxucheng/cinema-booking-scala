441a97554d0aa98d4cfda4913a1c4bc7bc71e543
Xu Cheng <charles.xu.cheng@gmail.com>
2024-05-01 08:23:44 +0800

    Add property based tests for SeatAllocationStrategy

---
+++ src/test/scala/cinema/SeatAllocationStrategyTest.scala
@@ -0,0 +1,27 @@
+package cinema
+
+import org.scalatest.matchers.should.Matchers.shouldBe
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+import org.scalacheck.Gen
+
+class SeatAllocationStrategyTest extends UnitSpec with ScalaCheckDrivenPropertyChecks {
+  "A reference point" when {
+    "calculated for a Row with even number of seats" should {
+      "never refer to an actual seat" in {
+        val evenRowSizes = for (n <- Gen.choose(1, 10000)) yield 2 * n
+        forAll(evenRowSizes) { n =>
+          SeatAllocationStrategy.refersToASeat(Row(1, n).midPoint) shouldBe false
+        }
+      }
+    }
+    "calculated for a Row with odd number of seats" should {
+      "always refer to an actual seat" in {
+        val oddRowSizes = for (n <- Gen.choose(0, 10000)) yield 2 * n + 1
+        forAll(oddRowSizes) { n =>
+          SeatAllocationStrategy.refersToASeat(Row(1, n).midPoint) shouldBe true
+        }
+      }
+    }
+  }
+
+}
